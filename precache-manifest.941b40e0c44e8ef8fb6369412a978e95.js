e82c909af1e7eaf5c91a0786f24be7bcfc0ae086
ismartfridge <44119770+ismartfridge@users.noreply.github.com>
2019-06-27 16:20:47 +0200

    Initial deploy commit

---
+++ precache-manifest.941b40e0c44e8ef8fb6369412a978e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54baa9e9163a9e13ed1c3bf8a624b07c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a374f2a06b362b86bc58",
+    "url": "/Portfolio/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a374f2a06b362b86bc58",
+    "url": "/Portfolio/static/js/main.1e76dbc9.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
